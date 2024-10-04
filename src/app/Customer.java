901bde785c15469cf518a1b324f46ceb649a4ec9
DrPaul1982 <avtoklavir@ukr.net>
2024-10-04 13:04:58 +0300

    Change toString to add date and time of output

--- src/app/Customer.java
+++ src/app/Customer.java
@@ -1,5 +1,8 @@
 package src.app;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Customer {
     private final String name;
     private final String phone;
@@ -18,4 +21,10 @@ public class Customer {
         return phone;
     }
 
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String currentTime = LocalDateTime.now().format(formatter);
+        return "Customer: " + name + ", phone: " + phone + ", time: " + currentTime;
+    }
 }
